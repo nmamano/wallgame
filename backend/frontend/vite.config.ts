2f92e918206da5f4e7547d444ac14ff0b85c7cc5
nmamano <nil.mamano@gmail.com>
2025-02-17 19:25:10 -0800

    Set up react proxy.
    Followed the instructions:
    
    0:38:29 - React Proxy
    
    from https://www.youtube.com/watch?v=jXyTIQOfTTk

--- backend/frontend/vite.config.ts
+++ backend/frontend/vite.config.ts
@@ -10,4 +10,25 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  // When developing locally, the frontend runs on port 5173 and the backend
+  // runs on port 3000.
+  // This makes it so, when developing locally, we can access the API routes
+  // through the frontend port, i.e., http://localhost:5173/api/.
+  // This matches the behavior of the production setup, where the frontend is
+  // served from the same origin as the API routes (it's actually kind of the
+  // opposite: Locally, everything goes through the frontend (due to this
+  // proxy). In production, everything goes through the backend (due to how we
+  // bundle it). But what matters is that, in both, there is a single origin).
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+      "/blog": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
+  },
 });
