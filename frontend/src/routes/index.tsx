c9865263940223a18adc3f18dcfd02155d06d93c
nmamano <nil.mamano@gmail.com>
2025-02-18 23:20:35 -0800

    Frontend: fetching (mock) puzzles from the puzzles API to the puzzles/ route.

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,29 +1,10 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-import { api } from "@/lib/api";
-import { useQuery } from "@tanstack/react-query";
-
 export const Route = createFileRoute("/")({
   component: Index,
 });
 
-async function getNumPuzzles() {
-  const res = await api.puzzles["count"].$get();
-  if (!res.ok) {
-    throw new Error("Server error: Failed to fetch number of puzzles");
-  }
-  const data = await res.json();
-  return data;
-}
-
 function Index() {
-  const { isPending, error, data } = useQuery({
-    queryKey: ["puzzles-count"],
-    queryFn: getNumPuzzles,
-  });
-
-  if (error) return "Server error: " + error.message;
-
   return (
     <>
       <div>
@@ -35,11 +16,6 @@ function Index() {
         <p>
           Visit the legacy version at{" "}
           <a href="https://www.wallwars.net/">wallwars.net</a>.
-        </p>
-      </div>
-      <div className="card">
-        <p>
-          API test: Number of puzzles: {isPending ? "Loading..." : data?.count}
         </p>
       </div>
     </>
--- frontend/src/routes/puzzles.tsx
+++ frontend/src/routes/puzzles.tsx
@@ -1,9 +1,44 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { api } from "@/lib/api";
+import { useQuery } from "@tanstack/react-query";
 
 export const Route = createFileRoute("/puzzles")({
   component: Puzzles,
 });
 
+async function getAllPuzzles() {
+  const res = await api.puzzles.$get();
+  if (!res.ok) {
+    throw new Error("Server error: Failed to fetch puzzles");
+  }
+  const data = await res.json();
+  return data;
+}
+
 function Puzzles() {
-  return <div>Show all puzzles</div>;
+  const { isPending, error, data } = useQuery({
+    queryKey: ["get-all-puzzles"],
+    queryFn: getAllPuzzles,
+  });
+
+  if (error) return "Server error: " + error.message;
+
+  return (
+    <>
+      <div>Show all puzzles</div>
+      <div>
+        {isPending
+          ? "Loading..."
+          : data?.puzzles.map((puzzle) => (
+              <div key={puzzle.id}>
+                <div>ID: {puzzle.id}</div>
+                <div>Title: {puzzle.title}</div>
+                <div>Author: {puzzle.author}</div>
+                <div>Rating: {puzzle.rating}</div>
+                <hr />
+              </div>
+            ))}
+      </div>
+    </>
+  );
 }
