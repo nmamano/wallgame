167434d3842229ed310677157bb0a25f292b6cb8
nmamano <nil.mamano@gmail.com>
2025-02-19 11:23:44 -0800

    Move the blog link to the nav bar

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -8,11 +8,7 @@ function Index() {
   return (
     <>
       <div>
-        <h1>Wall Game</h1>
         <p>Hello! Wall Game is under construction.</p>
-        <p>
-          Visit the <a href="/blog">blog</a>.
-        </p>
         <p>
           Visit the legacy version at{" "}
           <a href="https://www.wallwars.net/">wallwars.net</a>.
