ad8a302f1b0d9a4284619f02792fced1a2678cdf
nmamano <nil.mamano@gmail.com>
2025-02-19 11:23:44 -0800

    Move the blog link to the nav bar

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -17,6 +17,9 @@ function NavBar() {
       <Link to="/puzzles" className="[&.active]:font-bold">
         Puzzles
       </Link>
+      <a href="/blog" className="[&.active]:font-bold">
+        Blog
+      </a>
     </div>
   );
 }
