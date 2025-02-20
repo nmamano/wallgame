2cbfbe865bf531a6e900436e7e31ff9cd0cf0758
nmamano <nil.mamano@gmail.com>
2025-02-19 19:40:39 -0800

    Frontend: move logic for getting the user from the backend from profile.tsx to api.ts
    This is because we'll need to reuse this for multiple authenticated routes.

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,24 @@
 import { hc } from "hono/client";
 import { type ApiRoutes } from "@server/index";
+import { queryOptions } from "@tanstack/react-query";
 
 const client = hc<ApiRoutes>("/");
 
 export const api = client.api;
+
+export const userQueryOptions = queryOptions({
+  queryKey: ["get-current-user"],
+  queryFn: getCurrentUser,
+  staleTime: Infinity,
+});
+
+async function getCurrentUser() {
+  const res = await api.me.$get();
+  if (!res.ok) {
+    throw new Error(
+      `Server error: Failed to fetch current user: ${res.statusText}`
+    );
+  }
+  const data = await res.json();
+  return data;
+}
--- frontend/src/routes/profile.tsx
+++ frontend/src/routes/profile.tsx
@@ -1,27 +1,13 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { api } from "@/lib/api";
 import { useQuery } from "@tanstack/react-query";
+import { userQueryOptions } from "@/lib/api";
 
 export const Route = createFileRoute("/profile")({
   component: Profile,
 });
 
-async function getCurrentUser() {
-  const res = await api.me.$get();
-  if (!res.ok) {
-    throw new Error(
-      `Server error: Failed to fetch current user: ${res.statusText}`
-    );
-  }
-  const data = await res.json();
-  return data;
-}
-
 function Profile() {
-  const { isPending, error, data } = useQuery({
-    queryKey: ["get-current-user"],
-    queryFn: getCurrentUser,
-  });
+  const { isPending, error, data } = useQuery(userQueryOptions);
   if (isPending) {
     return <div>Loading...</div>;
   }
