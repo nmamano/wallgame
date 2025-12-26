b08b099a17f658058de2ba48ef41f1831a08d65d
nmamano <nil.mamano@gmail.com>
2025-12-25 16:34:42 -0800

    Sounds for game start/end.

--- frontend/src/lib/sounds.ts
+++ frontend/src/lib/sounds.ts
@@ -3,6 +3,8 @@ export const sounds = {
   pawnUndo: new Audio("/audio/pawn_undo.wav"),
   wall: new Audio("/audio/wall.wav"),
   wallUndo: new Audio("/audio/wall_undo.wav"),
+  gameStart: new Audio("/audio/game_start.mp3"),
+  gameEnd: new Audio("/audio/game_end.mp3"),
 };
 
 Object.values(sounds).forEach((a) => {
@@ -13,6 +15,8 @@ sounds.pawn.volume = 0.9;
 sounds.pawnUndo.volume = 0.1;
 sounds.wall.volume = 0.5;
 sounds.wallUndo.volume = 0.4;
+sounds.gameStart.volume = 0.7;
+sounds.gameEnd.volume = 0.7;
 
 export function play(a: HTMLAudioElement) {
   a.currentTime = 0;
